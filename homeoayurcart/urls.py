35d2b0edf6a090f3b6ca90a1bec9c2fae89771aa
rajilpatel <rajilpatel001@gmail.com>
2025-10-15 20:19:11 +0530

    removed invoice view

--- homeoayurcart/urls.py
+++ homeoayurcart/urls.py
@@ -10,7 +10,7 @@ from ecommerce.views import (
     product_details, update_cart, change_currency, toggle_wishlist,
     wishlist_view, cart, order_confirmation, past_orders, order_tracking,
     user_profile, search, get_address_details, checkout,
-    initiate_cashfree_payment, cashfree_callback, invoice,
+    initiate_cashfree_payment, cashfree_callback,
     privacy_policy, terms_and_conditions, shipping_and_delivery_policy, refund_and_cancellation_policy
 )
 
