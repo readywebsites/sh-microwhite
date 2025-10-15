3ae9cf8d9868e4f206e73797ef027e51df599dee
rajilpatel <rajilpatel001@gmail.com>
2025-10-15 17:26:51 +0530

    base dir

--- homeoayurcart/settings.py
+++ homeoayurcart/settings.py
@@ -15,10 +15,10 @@ import os  # Added this import for the os module
 
 import environ
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 
 # Quick-start development settings - unsuitable for production
