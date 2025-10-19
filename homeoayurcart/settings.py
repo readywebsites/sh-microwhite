32b29b82f9aa8279e3168e596d202a0d59bdd123
rajilpatel <rajilpatel001@gmail.com>
2025-10-19 14:39:52 +0530

    login sorted kadach

--- homeoayurcart/settings.py
+++ homeoayurcart/settings.py
@@ -167,7 +167,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
-LOGIN_URL = '/login/'
+LOGIN_URL = '/accounts/login/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
