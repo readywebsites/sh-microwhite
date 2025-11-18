383a137422bc57fd1050436e1822326d6b6ef53f
rajilpatel <rajilpatel001@gmail.com>
2025-11-18 13:07:05 +0530

    previous address and logo change

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -6,18 +6,20 @@ from ecommerce.models import UserProfile, Order, Address
 
 class AddressForm(forms.ModelForm):
     country = forms.ChoiceField(choices=[('IN', 'India')], initial='IN') # Set India as the only choice
+    existing_address = forms.ModelChoiceField(
+        queryset=Address.objects.none(),
+        empty_label="(Click to Choose an existing address)",
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
     class Meta:
         model = Address
-        fields = ['first_name', 'last_name', 'email', 'phone', 'address_line_1', 'country', 'state', 'city', 'zipcode']
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address_line_1', 'country', 'state', 'city', 'zipcode', 'existing_address']
 
     def __init__(self, user, *args, **kwargs):
         super(AddressForm, self).__init__(*args, **kwargs)
-        self.fields['existing_address'] = forms.ModelChoiceField(
-            queryset=Address.objects.filter(user=user),
-            empty_label="(Click to Choose an existing address)",
-            required=False,
-            widget=forms.Select(attrs={'class': 'form-control'})
-        )
+        self.fields['existing_address'].queryset = Address.objects.filter(user=user)
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
